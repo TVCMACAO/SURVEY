973bef92adfb499e4b3d2818897de378d6f01e6b
Survey App <survey-app@clinicamaicao.com>
2026-01-03 10:07:42 -0500

    Corregir error: agregar import faltante de faListUl en GroupAdminDashboard

--- frontend/survey-ui/src/components/GroupAdminDashboard.jsx
+++ frontend/survey-ui/src/components/GroupAdminDashboard.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faUsers, faFileLines, faChartBar, faUserPlus, faEdit, faTrash, 
   faEye, faPlus, faChevronLeft, faXmark, faShareNodes, faTable,
-  faDownload, faFileExcel, faArrowLeft, faUser
+  faDownload, faFileExcel, faArrowLeft, faUser, faListUl
 } from '@fortawesome/free-solid-svg-icons';
 import { authenticatedFetch } from '../auth';
 import GroupUsersManager from './GroupUsersManager';
